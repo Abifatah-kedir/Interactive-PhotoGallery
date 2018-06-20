a66281925d4baf023984e70e2a793e280e97aac2
codenerd1 <30376311+codenerd1@users.noreply.github.com>
2018-06-20 11:36:15 -0500

    finallly updated.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 
+
+/* filters throught the image and dislays images has a caption that matches 
+    the user input and hides rest of he images  */ 
+
  $('.myInput').keyup(event => {
-
+    // declares a variable that select input form the html file.
     const valueInput = $('.myInput').val().toLowerCase();
+    // selects and shows all images. 
     $('img').show();
-
+    // checks if the user enter something or not
     if (valueInput !== '') {
+        // if the user entered value, show only matches the image caption. 
         $('img').not('[title*="' + valueInput + '"]').hide();
     }
 });
